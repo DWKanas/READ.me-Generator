10e0a3ecfe7eb4701b0a1eaf2eada6cae668081f
DWKanas <dkanas0122@gmail.com>
2022-06-21 22:37:09 -0700

    added inquirer prompts

--- index.js
+++ index.js
@@ -14,10 +14,64 @@ const questions = [
     'What does the user need to know about using the repo?',
     'What does the user need to know about contributing to the repo?'
 ];
-
+//function to write and generate the README
 function writemdFile(data) {
     fs.writeFile('README.md', generate(data), (err) =>
         err ? console.error(err) : console.log('README Generated!')
     
     )
 }
+//array of inquirer prompts
+function init() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: questions[0],
+            name: 'username' 
+        },
+        {
+            type: 'input',
+            message: questions[1],
+            name: 'email' 
+        },
+        {
+            type: 'input',
+            message: questions[2],
+            name: 'project' 
+        },
+        {
+            type: 'input',
+            message: questions[3],
+            name: 'description' 
+        },
+        {
+            type: 'input',
+            message: questions[4],
+            name: 'license' 
+        },
+        {
+            type: 'input',
+            message: questions[5],
+            name: 'install' 
+        },
+        {
+            type: 'input',
+            message: questions[6],
+            name: 'test' 
+        },
+        {
+            type: 'input',
+            message: questions[7],
+            name: 'repo' 
+        },
+        {
+            type: 'input',
+            message: questions[8],
+            name: 'contribute' 
+        }
+    ])
+    .then(writemdFile)
+}
+
+init()
