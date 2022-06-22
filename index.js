00e5d684bb0a7eceda425b6c7eca6a3068c6a2fb
DWKanas <dkanas0122@gmail.com>
2022-06-21 22:23:31 -0700

    added questions array

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+//required packages
 const inquirer = require('inquirer');
+const fs = require('fs');
+
+//array of questions to ask
+const questions = [
+    'What is your GitHub username?',
+    'What is your email address?',
+    'What is your project\'s name?',
+    'Please write a short description of your project:',
+    'What kind of license should your project have?',
+    'What command should be run to install dependencies?',
+    'What command should be run to run test?',
+    'What does the user need to know about using the repo?',
+    'What does the user need to know about contributing to the repo?'
+];
+
